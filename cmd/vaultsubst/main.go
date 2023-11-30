aef1ef7316faeafcd2aad5703108d3c52a12363e
Samuel Kunst <samuel@kunst.me>
2023-11-30 21:10:13 +0100

    feat: add versioning

--- cmd/vaultsubst/main.go
+++ cmd/vaultsubst/main.go
@@ -14,28 +14,31 @@ var delimiter string
 
 var inPlace bool
 
-var app = &cli.App{
-	Name:            "vaultsubst",
-	Usage:           "inject and format vault secrets into files",
-	ArgsUsage:       "FILE [FILE...]",
-	Action:          runCmd,
-	HideHelpCommand: true,
-	Flags: []cli.Flag{
-		&cli.StringFlag{
-			Name:        "delimiter",
-			Aliases:     []string{"d", "delim"},
-			Value:       "@@",
-			Usage:       "delimiter to use for injections",
-			Destination: &delimiter,
+func New(version string) *cli.App {
+	return &cli.App{
+		Name:            "vaultsubst",
+		Usage:           "inject and format vault secrets into files",
+		ArgsUsage:       "FILE [FILE...]",
+		Action:          runCmd,
+		Version:         version,
+		HideHelpCommand: true,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "delimiter",
+				Aliases:     []string{"d", "delim"},
+				Value:       "@@",
+				Usage:       "delimiter to use for injections",
+				Destination: &delimiter,
+			},
+			&cli.BoolFlag{
+				Name:        "in-place",
+				Aliases:     []string{"i"},
+				Value:       false,
+				Usage:       "modify files in place",
+				Destination: &inPlace,
+			},
 		},
-		&cli.BoolFlag{
-			Name:        "in-place",
-			Aliases:     []string{"i"},
-			Value:       false,
-			Usage:       "modify files in place",
-			Destination: &inPlace,
-		},
-	},
+	}
 }
 
 func runCmd(ctx *cli.Context) error {
@@ -68,8 +71,4 @@ func runCmd(ctx *cli.Context) error {
 	}
 
 	return nil
-}
-
-func Run(args []string) error {
-	return app.Run(args)
 }
--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"os"
 
+	_ "embed"
 	cmd "github.com/toalaah/vaultsubst/cmd/vaultsubst"
 )
 
+//go:embed version.txt
+var version string
+
 func main() {
-	if err := cmd.Run(os.Args); err != nil {
+	if err := cmd.New(version).Run(os.Args); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
